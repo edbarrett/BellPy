373514dfc56445ef633fca54d598b20c7689d3bb
Elijah Barrett <elijahbarrett22@gmail.com>
2019-06-27 10:49:54 -0500

    first commit

---
+++ BellPy/BellPy.py
@@ -0,0 +1,15 @@
+
+'''
+This is the main package/library for this project. Eventually, it will hold all
+the functions necessary for solving multiple types of problems in the area of
+Digital and Analog Communications.
+
+    Future goals:
+        - Create a Energy function
+        - Create a Power function
+
+    Currently working on:
+        - 
+
+
+'''
